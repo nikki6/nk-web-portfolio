45ce8c05a6ba3c8b6a6c6bd78bdb52d1a3483c24
Nikki Keller <dkn5678@gmail.com>
2014-01-16 15:11:20 -0500

    This is the rugby website.

---
+++ RugbyWebsite/inc/header.php
@@ -0,0 +1,92 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>FWRFC</title>
+  <meta name="description" content="Frederick Women's Rugby Football Club">
+  <meta name="author" content="Nicole Keller">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link href='http://fonts.googleapis.com/css?family=Playfair+Display:400,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Questrial' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="css/reset.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+  <link rel="stylesheet" type="text/css" href="css/fontStyleSheet.css">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+
+  <script type="text/javascript" src="parallax/js/js.js"></script>
+  <script type="text/javascript" src="parallax/js/jquery.stellar.min.js"></script>
+  <script type="text/javascript" src="parallax/js/waypoints.min.js"></script>
+  <script type="text/javascript" src="parallax/js/jquery.easing.1.3.js"></script>
+  <script type="text/javascript">
+    $(document).ready(function(){ 
+        $("#myTab a").click(function(e){
+          e.preventDefault();
+          $(this).tab('show');
+        });
+    });
+  </script>
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+
+    <!-- Fixed navbar -->
+   <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">FWRFC</a>
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav navbar-right">
+      <li class="active"><a href="#">Home</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Team <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Schedule</a></li>
+          <li><a href="#">Roster</a></li>
+          <li><a href="#">Sevens</a></li>
+          <li><a href="#">Kit Bag</a></li>
+          <li><a href="#">Home Field</a></li>
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">News <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Team News</a></li>
+          <li><a href="#">Community Service</a></li>
+        </ul>
+      </li>
+    
+    <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Media <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Photos</a></li>
+          <li><a href="#">Promo</a></li>
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Misc <b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">About Rugby</a></li>
+          <li><a href="#">Contact Us</a></li>
+         
+        </ul>
+      </li>
+    </ul>
+  </div><!-- /.navbar-collapse -->
+</nav>
